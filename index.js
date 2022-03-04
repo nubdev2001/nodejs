98eae1fd15f239b6dc7c9add75edbfca56c4b9f2
nubdev2001 <62756404+nubdev2001@users.noreply.github.com>
2022-03-04 17:44:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res) => {
+app.get('/api',(req,res) => {
     const { name } = req.query
     res.json({
         name: !name ? "Hi" : `Hi ${name}`,
